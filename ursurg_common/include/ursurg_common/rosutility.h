434dfd002d4970af184856892864c4bfbbda92b0
Kim Lindberg Schwaner <kils@mmmi.sdu.dk>
2020-05-08 21:10:59 +0200

    common: Add ROS subscribe utility func taking msg as shared_ptr type

--- ursurg_common/include/ursurg_common/rosutility.h
+++ ursurg_common/include/ursurg_common/rosutility.h
@@ -17,3 +17,18 @@ ros::Subscriber mksub(ros::NodeHandle& nh,
                            ros::VoidConstPtr(),
                            transport_hints);
 }
+
+// The same, but take the message as a boost shared_ptr
+template<typename M, typename F>
+ros::Subscriber mksub_p(ros::NodeHandle& nh,
+                        const std::string& topic,
+                        uint32_t queue_size,
+                        F&& callback,
+                        const ros::TransportHints& transport_hints = ros::TransportHints())
+{
+    return nh.subscribe<M>(topic,
+                           queue_size,
+                           boost::function<void(const boost::shared_ptr<const M>&)>(std::move(callback)),
+                           ros::VoidConstPtr(),
+                           transport_hints);
+}
