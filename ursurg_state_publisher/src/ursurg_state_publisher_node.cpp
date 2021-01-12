95f9b4401bad3c3986a30a698dfd025785896ab1
Kim Lindberg Schwaner <kils@mmmi.sdu.dk>
2021-01-12 15:46:35 +0100

    state_publisher: Publish TCP pose and grasper angle and put TCP frame in URDF

--- ursurg_state_publisher/src/ursurg_state_publisher_node.cpp
+++ ursurg_state_publisher/src/ursurg_state_publisher_node.cpp
@@ -32,6 +32,10 @@
 *  POSSIBILITY OF SUCH DAMAGE.
 *********************************************************************/
 
+#include <ursurg_common/conversions/kdl.h>
+#include <ursurg_common/rosutility/subscription.h>
+
+#include <geometry_msgs/PoseStamped.h>
 #include <kdl_parser/kdl_parser.hpp>
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
@@ -39,76 +43,76 @@
 #include <tf2_ros/static_transform_broadcaster.h>
 #include <tf2_ros/transform_broadcaster.h>
 
+#include <kdl/chainfksolverpos_recursive.hpp>
 #include <kdl/tree.hpp>
 #include <urdf/model.h>
 
 #include <unordered_map>
 
+// TODO: Refactor so that RobotStatePublisher and PoseGraspPublisher utilizes the
+// same forward kinematics computations (transforms from each link to the next).
+
+using namespace std::string_literals;
+
 std::string slashStripped(const std::string& s)
 {
-    if ((s.size() >= 1) && s[0] == '/')
+    if (!s.empty() && s.front() == '/')
         return s.substr(1);
 
     return s;
 }
 
-class SegmentPair
+struct SegmentData
 {
-public:
-    SegmentPair(const KDL::Segment& segment, const std::string& root, const std::string& tip)
+    SegmentData(const KDL::Segment& segment, const std::string& root, const std::string& tip)
         : segment(segment)
         , root(slashStripped(root))
         , tip(slashStripped(tip))
+        , q(0)
     {}
 
     KDL::Segment segment;
     std::string root;
     std::string tip;
+    double q;
+    ros::Time stamp; // last time q was updated
+
+    geometry_msgs::TransformStamped transform() const
+    {
+        auto tf = tf2::kdlToTransform(segment.pose(q));
+        tf.header.stamp = stamp;
+        tf.header.frame_id = root;
+        tf.child_frame_id = tip;
+        return tf;
+    }
 };
 
 class RobotStatePublisher
 {
 public:
-    RobotStatePublisher()
+    explicit RobotStatePublisher(const KDL::Tree& tree)
     {
-        if (!model_.initParam("robot_description"))
-            throw std::runtime_error("Failed to load URDF model from parameter server");
-
-        if (!kdl_parser::treeFromUrdfModel(model_, tree_))
-            throw std::runtime_error("Failed to construct KDL tree from URDF model");
-
-        addChildren(tree_.getRootSegment());
+        addChildren(tree.getRootSegment());
     }
 
     void updateJointPositions(const sensor_msgs::JointState& m)
     {
-        if (m.name.size() != m.position.size()) {
-            ROS_WARN("Ignored an invalid JointState message");
-            return;
-        }
-
         for (std::size_t i = 0; i < m.name.size(); ++i) {
-            joint_positions_[m.name[i]] = m.position[i];
+            auto& sd = segments_moving_.at(m.name[i]);
+            sd.stamp = m.header.stamp;
+            sd.q = m.position[i];
         }
     }
 
     void publishTransforms(const ros::TimerEvent& e)
     {
         std::vector<geometry_msgs::TransformStamped> transforms;
+        transforms.reserve(segments_moving_.size());
 
-        for (auto const& [name, pos] : joint_positions_) {
-            auto sp = segments_.find(name);
-
-            if (sp != segments_.end()) {
-                auto tf = tf2::kdlToTransform(sp->second.segment.pose(pos));
-                tf.header.stamp = e.current_real;
-                tf.header.frame_id = sp->second.root;
-                tf.child_frame_id = sp->second.tip;
-                transforms.push_back(tf);
-            } else {
-                ROS_WARN_THROTTLE(10, "State for joint named '%s' was received but joint is not in URDF model", name.c_str());
-            }
-        }
+        for (auto const& [name, sd] : segments_moving_)
+            // Don't publish old data
+            if ((e.current_real - sd.stamp) < ros::Duration(1))
+                transforms.push_back(sd.transform());
 
         tf_broadcaster_.sendTransform(transforms);
     }
@@ -116,50 +120,120 @@ public:
     void publishFixedTransforms()
     {
         std::vector<geometry_msgs::TransformStamped> transforms;
+        transforms.reserve(segments_fixed_.size());
 
-        for (auto const& [name, sp] : segments_fixed_) {
-            auto tf = tf2::kdlToTransform(sp.segment.pose(0));
-            tf.header.stamp = ros::Time::now();
-            tf.header.frame_id = sp.root;
-            tf.child_frame_id = sp.tip;
-            transforms.push_back(tf);
-        }
+        for (auto const& [name, sd] : segments_fixed_)
+            transforms.push_back(sd.transform());
 
         static_tf_broadcaster_.sendTransform(transforms);
     }
 
 private:
-    void addChildren(const KDL::SegmentMap::const_iterator segment)
+    void addChildren(KDL::SegmentMap::const_iterator root_it)
     {
-        const auto& root_name = segment->second.segment.getName();
-
-        for (const auto& child_it : segment->second.children) {
+        for (const auto& child_it : root_it->second.children) {
             const auto& seg = child_it->second.segment;
-            const auto& jnt = seg.getJoint();
 
-            if (jnt.getType() == KDL::Joint::None) {
-                if (model_.getJoint(jnt.getName()) && model_.getJoint(jnt.getName())->type == urdf::Joint::FLOATING) {
-                    ROS_INFO("Floating joint. Not adding segment from %s to %s. This TF can not be published based on joint_states info", root_name.c_str(), seg.getName().c_str());
-                } else {
-                    segments_fixed_.insert({jnt.getName(), SegmentPair(seg, root_name, seg.getName())});
-                    ROS_DEBUG("Adding fixed segment from %s to %s", root_name.c_str(), seg.getName().c_str());
-                }
-            } else {
-                segments_.insert({jnt.getName(), SegmentPair(seg, root_name, seg.getName())});
-                ROS_DEBUG("Adding moving segment from %s to %s", root_name.c_str(), seg.getName().c_str());
-            }
+            if (seg.getJoint().getType() == KDL::Joint::None)
+                segments_fixed_.insert({seg.getJoint().getName(), {seg, root_it->second.segment.getName(), seg.getName()}});
+            else
+                segments_moving_.insert({seg.getJoint().getName(), {seg, root_it->second.segment.getName(), seg.getName()}});
 
             addChildren(child_it);
         }
     }
 
-    urdf::Model model_;
-    KDL::Tree tree_;
-    std::unordered_map<std::string, SegmentPair> segments_;
-    std::unordered_map<std::string, SegmentPair> segments_fixed_;
+    std::unordered_map<std::string, SegmentData> segments_moving_;
+    std::unordered_map<std::string, SegmentData> segments_fixed_;
     tf2_ros::TransformBroadcaster tf_broadcaster_;
     tf2_ros::StaticTransformBroadcaster static_tf_broadcaster_;
-    std::unordered_map<std::string, double> joint_positions_;
+};
+
+struct PoseGraspPublisher
+{
+public:
+    PoseGraspPublisher(const std::string& id, const KDL::Tree& tree)
+        : root_(id + "_ur_base_link")
+        , chain_(initChain(root_, id + "_tool_tcp0", tree))
+        , fk_solver_(chain_)
+        , q_current_(chain_.getNrOfJoints())
+        , q_current_yaw_{}
+    {
+        unsigned i = 0;
+
+        // Map to q_current by joint name
+        for (const auto& seg : chain_.segments)
+            if (seg.getJoint().getType() != KDL::Joint::None)
+                q_current_index_.insert({seg.getJoint().getName(), &q_current_(i++)});
+
+        // Also map grasper joint states (yaw1, yaw2) that are not part of chain
+        q_current_index_.insert({id + "_tool_yaw1", &q_current_yaw_[0]});
+        q_current_index_.insert({id + "_tool_yaw2", &q_current_yaw_[1]});
+
+        grasper_angle_name_ = id + "_tool_grasper_angle";
+
+        pub_pose_ = nh_.advertise<geometry_msgs::PoseStamped>("/" + id + "/tcp_pose_current", 1);
+        pub_grasp_ = nh_.advertise<sensor_msgs::JointState>("/" + id + "/grasp_current", 1);
+    }
+
+    void updateJointPositions(const sensor_msgs::JointState& m)
+    {
+        for (std::size_t i = 0; i < m.name.size(); ++i)
+            *q_current_index_.at(m.name[i]) = m.position[i];
+
+        last_update_ = m.header.stamp;
+    }
+
+    void publish(const ros::TimerEvent& e)
+    {
+        // Don't publish old data
+        if ((e.current_real - last_update_) > ros::Duration(1))
+            return;
+
+        KDL::Frame tf;
+        auto err = fk_solver_.JntToCart(q_current_, tf);
+
+        if (err != KDL::SolverI::E_NOERROR)
+            throw std::runtime_error("FK failed with error: "s + fk_solver_.strError(err));
+
+        geometry_msgs::PoseStamped m_pose;
+        m_pose.header.stamp = e.current_real;
+        m_pose.header.frame_id = root_;
+        m_pose.pose = convert_to<geometry_msgs::Pose>(tf);
+        pub_pose_.publish(m_pose);
+
+        sensor_msgs::JointState m_grasp;
+        m_grasp.header.stamp = e.current_real;
+        m_grasp.name.push_back(grasper_angle_name_);
+        m_grasp.position.push_back(q_current_yaw_[0] + q_current_yaw_[1]);
+        pub_grasp_.publish(m_grasp);
+    }
+
+private:
+    static KDL::Chain initChain(const std::string& root,
+                                const std::string& tip,
+                                const KDL::Tree& tree)
+    {
+        KDL::Chain chain;
+
+        if (!tree.getChain(root, tip, chain))
+            throw std::runtime_error("Failed to get kinematic chain between links: " + root + ", " + tip);
+
+        return chain;
+    }
+
+private:
+    std::string root_;
+    KDL::Chain chain_;
+    KDL::ChainFkSolverPos_recursive fk_solver_;
+    KDL::JntArray q_current_;
+    std::array<double, 2> q_current_yaw_;
+    std::unordered_map<std::string, double*> q_current_index_;
+    ros::Time last_update_;
+    ros::NodeHandle nh_;
+    ros::Publisher pub_pose_;
+    ros::Publisher pub_grasp_;
+    std::string grasper_angle_name_;
 };
 
 int main(int argc, char* argv[])
@@ -167,21 +241,91 @@ int main(int argc, char* argv[])
     ros::init(argc, argv, "ursurg_state_publisher");
     ros::NodeHandle nh;
     ros::NodeHandle nh_priv("~");
-    RobotStatePublisher robot_state_publisher;
-    std::list<ros::Subscriber> joint_state_subscribers;
 
-    // Can receive joint state messages from multiple sources
-    for (const auto& topic : nh_priv.param<std::vector<std::string>>("joint_state_topics", {})) {
-        joint_state_subscribers.push_back(nh.subscribe(topic,
-                                                       1,
-                                                       &RobotStatePublisher::updateJointPositions,
-                                                       &robot_state_publisher,
-                                                       ros::TransportHints().udp().tcp().tcpNoDelay()));
-    }
+    urdf::Model model;
 
-    auto timer = nh.createTimer(ros::Duration(1.0 / nh_priv.param("publish_frequency", 50.0)),
-                                &RobotStatePublisher::publishTransforms,
-                                &robot_state_publisher);
+    if (!model.initParam("robot_description"))
+        throw std::runtime_error("Failed to load URDF model from parameter server");
+
+    KDL::Tree tree;
+
+    if (!kdl_parser::treeFromUrdfModel(model, tree))
+        throw std::runtime_error("Failed to construct KDL tree from URDF model");
+
+    RobotStatePublisher robot_state_publisher(tree);
+    PoseGraspPublisher pose_pub_a("a", tree);
+    PoseGraspPublisher pose_pub_b("b", tree);
+
+    std::list<ros::Subscriber> subscribers = {
+        mksub<sensor_msgs::JointState>(
+            nh, "/a/ur/joint_states", 1, [&](const auto& m) {
+                robot_state_publisher.updateJointPositions(m);
+                pose_pub_a.updateJointPositions(m);
+            }, ros::TransportHints().udp().tcp().tcpNoDelay()),
+        mksub<sensor_msgs::JointState>(
+            nh, "/a/tool/joint_states", 1, [&](auto m) {
+                // Find indices of yaw1 and yaw2 joints
+                std::ptrdiff_t j = -1;
+                std::ptrdiff_t k = -1;
+
+                for (std::size_t i = 0; i < m.name.size(); ++i) {
+                    if (m.name[i].rfind("yaw1") != std::string::npos)
+                        j = i;
+                    else if (m.name[i].rfind("yaw2") != std::string::npos)
+                        k = i;
+                }
+
+                // Append yaw0 joint configuration between yaw1 and yaw2
+                if (j != -1 && k != -1) {
+                    m.name.push_back("a_tool_yaw0");
+                    m.position.push_back((m.position[j] - m.position[k]) / 2);
+                }
+
+                robot_state_publisher.updateJointPositions(m);
+                pose_pub_a.updateJointPositions(m);
+            }, ros::TransportHints().udp().tcp().tcpNoDelay()),
+        mksub<sensor_msgs::JointState>(
+            nh, "/b/ur/joint_states", 1, [&](const auto& m) {
+                robot_state_publisher.updateJointPositions(m);
+                pose_pub_b.updateJointPositions(m);
+            }, ros::TransportHints().udp().tcp().tcpNoDelay()),
+        mksub<sensor_msgs::JointState>(
+            nh, "/b/tool/joint_states", 1, [&](auto m) {
+                // Find indices of yaw1 and yaw2 joints
+                std::ptrdiff_t j = -1;
+                std::ptrdiff_t k = -1;
+
+                for (std::size_t i = 0; i < m.name.size(); ++i) {
+                    if (m.name[i].rfind("yaw1") != std::string::npos)
+                        j = i;
+                    else if (m.name[i].rfind("yaw2") != std::string::npos)
+                        k = i;
+                }
+
+                // Append yaw0 joint configuration between yaw1 and yaw2
+                if (j != -1 && k != -1) {
+                    m.name.push_back("b_tool_yaw0");
+                    m.position.push_back((m.position[j] - m.position[k]) / 2);
+                }
+
+                robot_state_publisher.updateJointPositions(m);
+                pose_pub_b.updateJointPositions(m);
+            }, ros::TransportHints().udp().tcp().tcpNoDelay()),
+    };
+
+    std::list<ros::Timer> timers = {
+        nh.createTimer(ros::Duration(1.0 / nh_priv.param("tf_publish_frequency", 50.0)),
+                       &RobotStatePublisher::publishTransforms,
+                       &robot_state_publisher),
+        nh.createTimer(ros::Duration(1.0 / nh_priv.param("pose_publish_frequency", 125.0)),
+                       &PoseGraspPublisher::publish,
+                       &pose_pub_a),
+        nh.createTimer(ros::Duration(1.0 / nh_priv.param("pose_publish_frequency", 125.0)),
+                       &PoseGraspPublisher::publish,
+                       &pose_pub_b),
+    };
+
+    robot_state_publisher.publishFixedTransforms();
     ros::spin();
     return EXIT_SUCCESS;
 }
